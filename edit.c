91c608b61a312401ad236b3eeb88716040d1b3bb
Martin Hardselius <martin@hardselius.dev>
2021-11-10 09:50:23 +0100

    Implement get_window_size

--- edit.c
+++ edit.c
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -13,7 +14,7 @@
 
 /*** data ***/
 
-struct editor_config 
+struct editor_config
 {
     struct termios orig_termios;
 };
@@ -63,6 +64,22 @@ char editor_read_key()
             die("read");
     }
     return c;
+}
+
+int get_window_size(int *rows, int *cols)
+{
+    struct winsize ws;
+
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+    {
+        return -1;
+    }
+    else
+    {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
 }
 
 /*** output ***/
